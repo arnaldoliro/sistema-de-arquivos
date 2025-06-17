6c68fa44d335c193cf6ffc2144126a0d9b359de5
Arnaldo Liro <dinholiro@gmail.com>
2025-06-17 14:26:52 -0300

    feat: adding animation and applying modal submit functionality

--- src/components/UploadModal/index.tsx
+++ src/components/UploadModal/index.tsx
@@ -2,6 +2,7 @@
 import { useModal } from "@/context/ModalContext"
 import { useEffect, useState } from "react"
 import { uploadFile } from "@/utils/api"
+import { motion } from "framer-motion"
 
 export default function Modal() {
     const { isOpen, closeModal } = useModal()
@@ -13,6 +14,17 @@ export default function Modal() {
     const [lotacao, setLotacao] = useState('')
     const [arquivo, setArquivo] = useState<File | null>(null)
     const [mensagem, setMensagem] = useState('')
+    const [success, setSuccess] = useState(false)
+    
+    const resetForm = () => {
+      setNome('')
+      setDescricao('')
+      setCategoria('documento')
+      setLotacao('')
+      setArquivo(null)
+      setMensagem('')
+    }
+
 
     // Função para pegar e enviar os dados do formulário
     const handleSubmit = async (e: React.FormEvent) => {
@@ -27,16 +39,15 @@ export default function Modal() {
     const base64 = Buffer.from(fileBuffer).toString('base64')
 
     try {
-      await uploadFile({
-        nome,
-        descricao,
-        categoria,
-        lotacao,
-        conteudo: base64,
-      })
-      setMensagem('Arquivo enviado com sucesso!')
+      await uploadFile({ nome, descricao, categoria, lotacao, conteudo: base64 })
+      setSuccess(true)
+      setTimeout(() => {
+        closeModal()
+        setSuccess(false)
+        resetForm()
+      }, 2000)
     } catch (err: any) {
-      setMensagem(`Erro: ${err.message}`)
+        setMensagem(`Erro: ${err.message}`)
     }
   }
 
@@ -46,7 +57,7 @@ export default function Modal() {
       setShow(true)
      } else {
     
-      const timeout = setTimeout(() => setShow(false), 200)
+      const timeout = setTimeout(() => setShow(false), 100)
       return () => clearTimeout(timeout)
      }
     }, [isOpen])
@@ -55,15 +66,17 @@ export default function Modal() {
     if (!isOpen && !show) return null
 
     return (
+        
         <div className={`fixed inset-0 bg-[#00000079] items-center flex justify-center z-50 tion-opacity duration-500 ${isOpen ? "bg-black/50 opacity-100" : "opacity-0 pointer-events-none"}`}>
-        <div className={`bg-white rounded-lg shadow-xl w-full max-w-md p-6  transform transition-all duration-500 ${isOpen ? "scale-100 opacity-100 translate-y-0" : "scale-95 opacity-0 translate-y-4"}`}>
+         {!success && (
+          <div className={`bg-white rounded-lg shadow-xl w-full max-w-md p-6  transform transition-all duration-500 ${isOpen ? "scale-100 opacity-100 translate-y-0" : "scale-95 opacity-0 translate-y-4"}`}>
             <div className="flex justify-between items-center mb-4">
                 <h3 className="text-xl font-bold text-gray-800">Enviar Novo Arquivo</h3>
                 <button onClick={closeModal} className="text-gray-500 hover:text-gray-700 cursor-pointer">
                     <i className="fas fa-times"></i>
                 </button>
             </div>
-            <form className="space-y-4" onSubmit={handleSubmit}>
+             <form className="space-y-4" onSubmit={handleSubmit}>
                 <div>                   
                     <label className="block text-gray-700 mb-2">Nome do Arquivo</label>
                     <input value={nome} onChange={e => setNome(e.target.value)} type="text" required className="duration-300 w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" />
@@ -90,12 +103,18 @@ export default function Modal() {
                 <div>
                     <label className="block text-gray-700 mb-2">Arquivo</label>
                     <div className="border-2 border-dashed border-gray-300 rounded-lg p-6 text-center">
-                        <input type="file" onChange={e => setArquivo(e.target.files?.[0] || null)} className="hidden" />
-                        <label className="cursor-pointer">
+                        <input id="file-upload" type="file" onChange={e => setArquivo(e.target.files?.[0] || null)} className="hidden"/>
+                        <label htmlFor="file-upload" className="cursor-pointer">
                             <i className="fas fa-cloud-upload-alt text-4xl text-blue-500 mb-2"></i>
                             <p className="text-gray-700">Clique para selecionar ou arraste um arquivo</p>
                             <p className="text-sm text-blue-600 mt-2"></p>
                         </label>
+                        {arquivo && (
+                            <div className="mt-4">
+                                <p className="text-sm text-gray-600">Arquivo selecionado:</p>
+                                <p className="text-blue-600 font-medium break-all">{arquivo.name}</p>
+                            </div>
+                        )}                        
                     </div>
                 </div>
                 <div className="flex justify-end">
@@ -103,7 +122,28 @@ export default function Modal() {
                     <button type="submit" className="bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg cursor-pointer duration-300">Enviar</button>
                 </div>
             </form>
+          </div>
+         )}
+         <div>
+            {success && (
+                <div className="bg-white rounded-lg flex flex-col justify-center items-center text-center shadow-xl w-full max-w-md p-6">
+                    <div className="flex justify-between items-center mb-4">
+                        <h3 className="text-xl font-bold text-gray-800">Enviar Novo Arquivo</h3>
+                    </div>
+                    <motion.div
+                        initial={{ scale: 0 }}
+                        animate={{ scale: 1 }}
+                        exit={{ scale: 0 }}
+                        className="items-center text-center justify-center h-24"
+                    >
+                        <div className="w-16 h-16 rounded-full bg-green-500 flex items-center text-center mx-auto mt-3 justify-center">
+                            <i className="fas fa-check text-white text-2xl"></i>
+                        </div>
+                    </motion.div>
+                </div>
+            )}
+         </div>
         </div>
-    </div>  
+              
     )
 }
